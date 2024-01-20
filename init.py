8a7ecc8c537403b9eca6edb23653fe0fefa6b566
Bertin-fap <francois.bertin7@wanadoo.fr>
2024-01-20 14:03:43 +0100

    first commit

---
+++ init.py
@@ -0,0 +1,22 @@
+def download_nltk_data():
+   """
+   on big master
+
+   """
+
+    # Standard library imports
+
+    import os # on master
+ 
+    # 3rd party imports
+    import nltk
+ 
+    for nltk_path in nltk.data.path: # if data are already downloaded than exit  
+        if os.path.exists(nltk_path): return
+    
+    # no nltk data found then download them	
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('punkt')
+    nltk.download('wordnet')
+
+download_nltk_data()
